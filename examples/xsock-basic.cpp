b7e06994cfb6318ffc8d5c45d41b8f5e94690340
Wilson Birney <wpbirney@gmail.com>
2016-06-30 14:03:13 -0400

    updated examples

--- examples/xsock-basic.cpp
+++ examples/xsock-basic.cpp
@@ -3,27 +3,15 @@
 
 int main()
 {
+	std::vector<net::endpoint> eps = net::endpoint::getEndpoints("www.google.com", 80, net::sock::stream);
+	for( net::endpoint &i : eps )	{
+		std::cout << i.asString() << std::endl;
+	}
+
 	net::socket v6s ( net::af::inet6, net::sock::dgram, 4444 );
 
 	std::cout << "listening at: " << v6s.getlocaladdr().asString() << std::endl;
-	/*
-	addrinfo info;
-	info.ai_family = AF_UNSPEC;
-	info.ai_socktype = SOCK_DGRAM;
-	info.ai_flags = 0;
-	info.ai_protocol = 0;
 
-	addrinfo *res, *rp;
-
-	getaddrinfo("192.168.1.5", NULL, &info, &res);
-
-	for(rp = res; rp != NULL; rp = rp->ai_next)	{
-		std::vector<char> buffer ( INET6_ADDRSTRLEN );
-		getnameinfo(rp->ai_addr, rp->ai_addrlen, buffer.data(), buffer.size(), NULL, 0, 0);
-		std::cout << std::string( buffer.begin(), buffer.end() ) << std::endl;
-	}
-	return -1;
-	*/
 	std::string buff;
 	net::endpoint pp;
 	v6s.recvfrom( &buff, 512, &pp );
