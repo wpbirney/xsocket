35a3cae99293adea71ba1aa59c6e35757e3eeda6
Wilson Birney <wpbirney@gmail.com>
2016-07-04 18:48:22 -0400

    updated examples

--- examples/ipecho.cpp
+++ examples/ipecho.cpp
@@ -4,15 +4,20 @@
 
 
 int main()	{
+
+	//init only required for windows, no-op on *nix
 	net::init();
 
+	//create a tcp inetV4 socket and bind it to port 8080
 	net::socket sock( net::af::inet, net::sock::stream, 8080 );
 
+	//make sure socket creation and binding did not fail
 	if( !sock.isValid() )	{
-		std::cout << "error creating socket" << std::endl;
+		std::cerr << "error creating socket" << std::endl;
 		return -1;
 	}
 
+	//we must call listen before a call to accept
 	sock.listen( 5 );
 
 	std::cout << "listening on port: " << sock.getlocaladdr().getPort() << std::endl;
@@ -21,18 +26,16 @@ int main()	{
 	net::endpoint remoteAddr;
 
 	while( true )	{
+
+		//accept() returns the fd which you can directly copy into a net::socket
 		client = sock.accept( &remoteAddr );
+
+		//verify that sock.accept did not fail
 		if(client.isValid())	{
 			std::stringstream ss;
-			ss << "HTTP/1.1 200 OK" << std::endl
-			   << "Content-Type: text/html"
-			   << std::endl
-			   << std::endl
-			   << remoteAddr.asString()
-			   << std::endl;
+			ss << remoteAddr.getIP() << std::endl;
 			std::string msg = ss.str();
 			client.send( &msg );
-			client.shutdown( net::shut::wr );
 			client.close();
 		}
 	}
