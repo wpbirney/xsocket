a6990120fcdade1a09facfc4d4c4379bfec02a2a
Wilson Birney <wpbirney@gmail.com>
2017-06-26 13:03:57 -0400

    added new udp-ipecho example

---
+++ examples/udp-ipecho.cpp
@@ -0,0 +1,39 @@
+/*
+ *	the following example demonstrates a very basic tcp ip echo server using the xsocket library
+ */
+
+#include "../xsocket.hpp"
+#include <iostream>
+
+int main()	{
+
+	//init only required for windows, no-op on *nix
+	net::init();
+
+	//create a udp inetV4 socket and bind it to port 8080
+	net::socket sock( net::af::inet, net::sock::dgram, 8080 );
+
+	//make sure socket creation and binding did not fail
+	if( !sock.isValid() )	{
+		std::cerr << "error creating socket" << std::endl;
+		return -1;
+	}
+
+	std::cout << "listening on port: " << sock.getlocaladdr().getPort() << std::endl;
+
+	net::endpoint remoteAddr;
+	std::string buf;
+
+	while( true )	{
+
+		//accept() returns the fd which you can directly copy into a net::socket
+		int r = sock.recvfrom( &buf, 1024, &remoteAddr );
+
+		if( r > 0 && buf == "ip\n" )	{
+			std::string msg = remoteAddr.getIP();
+			sock.sendto( &msg, remoteAddr );
+		}
+	}
+
+	return 0;
+}
